e02622fa2834c7171916b33b768fc1f737ad69d1
Ygor Portes <ygorsportes@gmail.com>
2025-09-17 16:50:08 -0300

    feat: adicionadas configurações de segurança

---
+++ src/main/java/com/ygorportes/agendadortarefas/infrastructure/client/UsuarioClient.java
@@ -0,0 +1,15 @@
+package com.ygorportes.agendadortarefas.infrastructure.client;
+
+import com.ygorportes.agendadortarefas.business.dto.UsuarioDTO;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(name = "usuario", url = "${usuario.url}")
+public interface UsuarioClient {
+
+    @GetMapping
+    UsuarioDTO buscaUsuarioPorEmail(@RequestParam("email") String email,
+                                    @RequestHeader("Authorization") String token);
+}
---
+++ src/main/java/com/ygorportes/agendadortarefas/infrastructure/security/JwtRequestFilter.java
@@ -0,0 +1,46 @@
+package com.ygorportes.agendadortarefas.infrastructure.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+public class JwtRequestFilter extends OncePerRequestFilter {
+
+    private final JwtUtil jwtUtil;
+    private final UserDetailsServiceImpl userDetailsService;
+
+    public JwtRequestFilter(JwtUtil jwtUtil, UserDetailsServiceImpl userDetailsService) {
+        this.jwtUtil = jwtUtil;
+        this.userDetailsService = userDetailsService;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
+            throws ServletException, IOException {
+
+        final String authorizationHeader = request.getHeader("Authorization");
+
+        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+            final String token = authorizationHeader.substring(7);
+            final String username = jwtUtil.extrairEmailToken(token);
+
+            if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+                UserDetails userDetails = userDetailsService.carregaDadosUsuario(username, authorizationHeader);
+                if (jwtUtil.validateToken(token, username)) {
+                    UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
+                            userDetails, null, userDetails.getAuthorities());
+                    SecurityContextHolder.getContext().setAuthentication(authentication);
+                }
+            }
+        }
+
+        chain.doFilter(request, response);
+    }
+}
