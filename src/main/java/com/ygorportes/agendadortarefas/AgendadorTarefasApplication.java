e52c90d1e7786529082fe601c2c2d87d74a0a4cd
Ygor Portes <ygorsportes@gmail.com>
2025-09-17 15:16:32 -0300

    feat: commit inicial

---
+++ src/main/java/com/ygorportes/agendadortarefas/AgendadorTarefasApplication.java
@@ -0,0 +1,13 @@
+package com.ygorportes.agendadortarefas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendadorTarefasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgendadorTarefasApplication.class, args);
+    }
+
+}
