98821c6f10cf6b477f8eda1c489f93d95490feb1
Ygor Portes <ygorsportes@gmail.com>
2025-09-17 16:57:34 -0300

    correção de apontamentos

--- src/main/java/com/ygorportes/agendadortarefas/infrastructure/security/SecurityConfig.java
+++ src/main/java/com/ygorportes/agendadortarefas/infrastructure/security/SecurityConfig.java
@@ -16,10 +16,10 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 public class SecurityConfig {
 
     private final JwtUtil jwtUtil;
-    private final UserDetailsService userDetailsService;
+    private final UserDetailsServiceImpl userDetailsService;
 
     @Autowired
-    public SecurityConfig(JwtUtil jwtUtil, UserDetailsService userDetailsService) {
+    public SecurityConfig(JwtUtil jwtUtil, UserDetailsServiceImpl userDetailsService) {
         this.jwtUtil = jwtUtil;
         this.userDetailsService = userDetailsService;
     }
